745ab674b6e10b19458c919f48c3a10fd37fd00c
Mirza Rizky Maulana <mirzarizky@gmail.com>
2018-04-17 21:48:09 +0700

    Redirect https, timezone : Asia/Jakarta, auth (login & logout) no register.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        if((env('APP_ENV') == 'local') && env('DB_CONNECTION') == 'mysql') {
+            Schema::defaultStringLength(191); //MySQL older than the 5.7.7 release or MariaDB older than the 10.2.2 release
+        }
+        if(env('REDIRECT_HTTPS')) {
+            URL::forceScheme('https'); //for heroku deployment
+        }
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true); //for heroku deployment
+        }
     }
 }
